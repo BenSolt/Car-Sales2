6ff0bc62d10178671b4b9167490906c8ba631d58
benjamin Soltgit config user.email bensolt@gmail.com <bensolt@gmail.com>
2019-11-06 11:52:53 -0500

    added provider to main index.js

--- src/index.js
+++ src/index.js
@@ -2,8 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+//Import Create Store
+import { createStore } from "redux";
+//Import Provider
+import { Provider } from "react-redux";
+//Import Reducer from file path
+import { carReducer } from "./reducers";
+
+
 import 'bulma/css/bulma.css';
 import './styles.scss';
 
+const store = createStore(carReducer);
+
+
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(
+<Provider store={store}>
+<App />
+</Provider>, rootElement);
