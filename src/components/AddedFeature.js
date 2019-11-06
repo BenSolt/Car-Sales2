ca638453570e4bfe2476d81b92ae603abb1ad3b4
benjamin Soltgit config user.email bensolt@gmail.com <bensolt@gmail.com>
2019-11-06 13:00:39 -0500

    adding remove item to added feature

--- src/components/AddedFeature.js
+++ src/components/AddedFeature.js
@@ -7,10 +7,10 @@ const AddedFeature = props => {
   return (
     <li>
       {/* Add an onClick to run a function to remove a feature */}
-      <button className="button" onClick={() => props.removeItem(props.remove)}>X</button>
+      <button className="button" >X</button>
       {props.feature.name}
     </li>
   );
 };
 
-export default  connect(null, {removeItem}) (AddedFeature);
+export default  AddedFeature;
